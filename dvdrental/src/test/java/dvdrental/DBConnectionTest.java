91462b9580a3a1827d5ee5577318c02795865bda
danishbangash05 <danish.bangash05@gmail.com>
2021-09-08 12:44:29 -0400

    DataBaseConnectionFrameWork

---
+++ dvdrental/src/test/java/dvdrental/DBConnectionTest.java
@@ -0,0 +1,34 @@
+package dvdrental;
+
+import dbconnectionsetup.DBSetup;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnectionTest extends DBSetup {
+    @Test
+    public void dbTest() throws SQLException {
+
+        String query = "SELECT first_name, last_name FROM public.actor";
+
+        Connection connect = setUp();
+
+        Statement statement = connect.createStatement();
+
+        ResultSet resultSet = statement.executeQuery(query);
+
+        while (resultSet.next()) {
+            String actual = resultSet.getString("first_name");
+            String act = resultSet.getString("last_name");
+            Assert.assertEquals(actual,"Penelope" ,act);
+            System.out.println(actual + " " + act);
+            break;
+
+        }
+
+    }
+}
